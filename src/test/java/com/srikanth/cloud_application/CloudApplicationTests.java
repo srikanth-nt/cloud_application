9757af4d260ce5097933ea522c53650405d62def
Srikanth NT <630886@cognizant.com>
2022-02-18 19:59:04 +0530

    Initial Cut

---
+++ src/test/java/com/srikanth/cloud_application/CloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srikanth.cloud_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
